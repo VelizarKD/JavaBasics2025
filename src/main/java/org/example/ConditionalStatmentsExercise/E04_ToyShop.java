d991a23826516f21d392a6f45897b968767a965a
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-03-26 10:32:21 +0200

    few more exercises

---
+++ src/main/java/org/example/ConditionalStatmentsExercise/E04_ToyShop.java
@@ -0,0 +1,9 @@
+package org.example.ConditionalStatmentsExercise;
+
+import java.util.Scanner;
+
+public class E04_ToyShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
