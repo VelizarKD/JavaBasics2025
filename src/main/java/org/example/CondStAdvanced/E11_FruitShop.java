46bfbce370564b23ff2aa21836cf4fa26e4f8c08
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-03-27 12:33:44 +0200

    few more exercises

--- src/main/java/org/example/CondStAdvanced/E11_FruitShop.java
+++ src/main/java/org/example/CondStAdvanced/E11_FruitShop.java
@@ -1,4 +1,71 @@
 package org.example.CondStAdvanced;
 
+import java.util.Scanner;
+
 public class E11_FruitShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String product = scanner.nextLine();
+        String day = scanner.nextLine();
+        double quantity = Double.parseDouble(scanner.nextLine());
+
+        double price = 0;
+        double totalPrice = 0;
+
+        switch (day) {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                if (product.equals("banana")) {
+                    price = 2.5;
+                } else if (product.equals("apple")) {
+                    price = 1.2;
+                } else if (product.equals("orange")) {
+                    price = 0.85;
+                } else if (product.equals("grapefruit")) {
+                    price = 1.45;
+                } else if (product.equals("kiwi")) {
+                    price = 2.70;
+                } else if (product.equals("pineapple")) {
+                    price = 5.50;
+                } else if (product.equals("grapes")) {
+                    price = 3.85;
+                } else {
+                    System.out.println("error");
+                }
+                break;
+            case "Saturday":
+            case "Sunday":
+                if (product.equals("banana")) {
+                    price = 2.7;
+                } else if (product.equals("apple")) {
+                    price = 1.25;
+                } else if (product.equals("orange")) {
+                    price = 0.90;
+                } else if (product.equals("grapefruit")) {
+                    price = 1.60;
+                } else if (product.equals("kiwi")) {
+                    price = 3;
+                } else if (product.equals("pineapple")) {
+                    price = 5.60;
+                } else if (product.equals("grapes")) {
+                    price = 4.20;
+                } else {
+                    System.out.println("error");
+                }
+
+                break;
+            default:
+                System.out.println("error");
+                break;
+        }
+        totalPrice = price * quantity;
+        if (totalPrice > 0) {
+            System.out.printf("%.2f", totalPrice);
+        }
+
+    }
 }
--- src/main/java/org/example/CondStAdvanced/E12_TradeCommissions.java
+++ src/main/java/org/example/CondStAdvanced/E12_TradeCommissions.java
@@ -1,4 +1,59 @@
 package org.example.CondStAdvanced;
 
+import java.util.Scanner;
+
 public class E12_TradeCommissions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         String city = scanner.nextLine();
+         double sales = Double.parseDouble(scanner.nextLine());
+
+         boolean isValidData = true;
+         double commission = 0;
+
+        if (city.equals("Sofia")) {
+            if (sales >= 0 && sales <= 500) {
+                commission = sales * 0.05;
+            } else if (sales > 500 && sales <= 1000) {
+                commission = sales * 0.07;
+            } else if (sales > 1000 && sales <= 10000) {
+                commission = sales * 0.08;
+            } else if (sales > 10000) {
+                commission = sales * 0.12;
+            } else {
+                isValidData = false;
+            }
+        } else if (city.equals("Varna")) {
+            if (sales >= 0 && sales <= 500) {
+                commission = sales * 0.045;
+            } else if (sales > 500 && sales <= 1000) {
+                commission = sales * 0.075;
+            } else if (sales > 1000 && sales <= 10000) {
+                commission = sales * 0.10;
+            } else if (sales > 10000) {
+                commission = sales * 0.13;
+            } else {
+                isValidData = false;
+            }
+        } else if (city.equals("Plovdiv")) {
+            if (sales >= 0 && sales <= 500) {
+                commission = sales * 0.055;
+            } else if (sales > 500 && sales <= 1000) {
+                commission = sales * 0.08;
+            } else if (sales > 1000 && sales <= 10000) {
+                commission = sales * 0.12;
+            } else if (sales > 10000) {
+                commission = sales * 0.145;
+            } else {
+                isValidData = false;
+            }
+        } else {
+            isValidData = false;
+        }
+         if (isValidData) {
+             System.out.printf("%.2f", commission);
+         } else {
+             System.out.println("error");
+         }
+    }
 }
