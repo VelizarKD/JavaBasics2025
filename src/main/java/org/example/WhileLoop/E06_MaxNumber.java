8288c1537062a65d33dfd6247f6e2a10ee20e9cc
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-04-02 23:00:05 +0300

    few more exercises

---
+++ src/main/java/org/example/WhileLoop/E06_MaxNumber.java
@@ -0,0 +1,22 @@
+package org.example.WhileLoop;
+
+import java.util.Scanner;
+
+public class E06_MaxNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int maxNumber = Integer.MIN_VALUE;
+
+        while (!input.equals("Stop")) {
+            int number = Integer.parseInt(input);
+
+            if (number > maxNumber) {
+                maxNumber = number;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println(maxNumber);
+    }
+}
