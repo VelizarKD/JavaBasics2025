ca8c214b7d842b746ed0f03a5df7b01e22fa35a8
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-04-16 21:14:54 +0300

    few while loop exercises

--- src/main/java/org/example/WhileLoopExercise/E05_Coins.java
+++ src/main/java/org/example/WhileLoopExercise/E05_Coins.java
@@ -6,6 +6,34 @@ public class E05_Coins {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        double change = Double.parseDouble(scanner.nextLine());
 
+        change = change * 100;
+        int coinsCount = 0;
+
+        while (change > 0) {
+            if (change >= 200) {
+                change -= 200;
+            } else if (change >= 100) {
+                change -= 100;
+            } else if (change >= 50) {
+                change -= 50;
+            } else if (change >= 20) {
+                change -= 20;
+            } else if (change >= 10) {
+                change -= 10;
+            } else if (change >= 5) {
+                change -= 5;
+            } else if (change >= 2) {
+                change -= 2;
+            } else if (change >= 1) {
+                change -= 1;
+            } else {
+                break;
+            }
+
+            coinsCount++;
+        }
+        System.out.println(coinsCount);
     }
 }
