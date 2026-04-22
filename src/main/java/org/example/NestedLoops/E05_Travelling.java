df00cad8713ce9f3703992a251acfd3e952a7e20
VelizarKD <79282909+VelizarKD@users.noreply.github.com>
2026-04-22 23:01:42 +0300

    few nested loops exercises

--- src/main/java/org/example/NestedLoops/E05_Travelling.java
+++ src/main/java/org/example/NestedLoops/E05_Travelling.java
@@ -1,4 +1,27 @@
 package org.example.NestedLoops;
 
+import java.util.Scanner;
+
 public class E05_Travelling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String destination = scanner.nextLine();
+
+        while (!destination.equals("End")) {
+            double neededMoney = Double.parseDouble(scanner.nextLine());
+            double budget = 0;
+
+            while (neededMoney > budget) {
+                double currentMoney = Double.parseDouble(scanner.nextLine());
+
+                budget += currentMoney;
+            }
+
+            System.out.printf("Going to %s!%n", destination);
+
+
+            destination = scanner.nextLine();
+        }
+    }
 }
